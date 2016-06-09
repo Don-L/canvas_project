ecccaf3e1e5a6574e7a32f7b1ac55cf7254d580a
Donald Lessells <donald.lessells@gmail.com>
2016-06-09 15:03:41 +0100

    initial commit of canvas project

---
+++ public/index.js
@@ -0,0 +1,49 @@
+// window.onload = function() {
+//   console.log('App started');
+//   var canvas = document.getElementById('main');
+//   console.log(canvas);
+
+  //we need to get a context for the canvas. we can draw on the context. we can have multiple contexts
+  // var context = canvas.getContext('2d');
+  // context.lineWidth = 10;
+  // context.fillStyle = 'red';
+  // context.strokeStyle = 'orange';
+  // context.fillRect(10,10,50,50); // top left corner x, top left corner y, width, height
+
+  // context.beginPath();
+  // context.lineWidth = 10;
+  // context.moveTo(100,100);
+  // context.lineTo(100,150);
+  // context.stroke(); //creates the line described above
+
+  // var drawCircle = function(x,y) {
+  //   context.beginPath();
+  //   context.arc(x,y, 60, 0, Math.PI*2);
+  //   context.stroke();
+  //   context.fill();
+  // };
+ 
+  // canvas.onclick = function(event) {
+  //   drawCircle(event.x, event.y);
+  // }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
