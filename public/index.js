a1f144d11aa742c55d320811b5d0c34affda04ad
Donald Lessells <donald.lessells@gmail.com>
2016-06-09 15:18:56 +0100

    added some stuff

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
-// window.onload = function() {
-//   console.log('App started');
-//   var canvas = document.getElementById('main');
-//   console.log(canvas);
+window.onload = function() {
+  console.log('App started');
+  var canvas = document.getElementById('main');
+  console.log(canvas);
 
   //we need to get a context for the canvas. we can draw on the context. we can have multiple contexts
-  // var context = canvas.getContext('2d');
-  // context.lineWidth = 10;
-  // context.fillStyle = 'red';
+  var context = canvas.getContext('2d');
+  context.lineWidth = 10;
+  // context.fillStyle = 'black';
   // context.strokeStyle = 'orange';
   // context.fillRect(10,10,50,50); // top left corner x, top left corner y, width, height
 
@@ -16,18 +16,24 @@
   // context.lineTo(100,150);
   // context.stroke(); //creates the line described above
 
-  // var drawCircle = function(x,y) {
-  //   context.beginPath();
-  //   context.arc(x,y, 60, 0, Math.PI*2);
-  //   context.stroke();
-  //   context.fill();
-  // };
+  var drawCircle = function(x,y, r) {
+    context.beginPath();
+    context.arc(x,y, r, 0, Math.PI*2);
+    context.stroke();
+    context.fill();
+  };
  
-  // canvas.onclick = function(event) {
-  //   drawCircle(event.x, event.y);
-  // }
+  canvas.onclick = function(event) {
+    drawCircle(event.x, event.y);
+  }
 
-// }
+  var form = document.getElementById('circleForm');
+
+  form.onsubmit = function(event) {
+
+  }
+
+}
 
 
 
